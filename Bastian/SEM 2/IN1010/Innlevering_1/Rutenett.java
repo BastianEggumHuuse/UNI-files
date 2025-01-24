22fe9ad8472d2f76deda51e198e2e2585168127d
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-01-24 21:09:02 +0100

    Begynnelse på Conway's Game of Life (Innlevering 1 IN1010))

---
+++ Bastian/SEM 2/IN1010/Innlevering_1/Rutenett.java
@@ -0,0 +1,11 @@
+class Rutenett
+{
+    int AntRader;
+    int AntKolonner;
+
+    public Rutenett(int Rader,int Kolonner)
+    {
+        AntRader = Rader;
+        AntKolonner = Kolonner
+    }
+}
