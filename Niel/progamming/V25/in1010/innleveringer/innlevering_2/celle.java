d8bdfde3f3df148c24d7cc3c7fcd345115e705e9
niel-carlsen <carlsenniel2005@gmail.com>
2025-01-24 22:44:07 +0100

    innlevering 2
    ferdig med del 1 av oppgave :)

---
+++ Niel/progamming/V25/in1010/innleveringer/innlevering_2/celle.java
@@ -0,0 +1,71 @@
+class celle {
+
+    boolean levende;
+    celle[] naboer;
+    int AntallNaboer;
+    int antLevendeNaboer;
+
+    public celle(){
+        levende = false;
+        AntallNaboer = 0;
+        antLevendeNaboer = 0;
+        naboer = new celle[8];
+    }
+
+    public void settDod(){
+        levende = false;
+    }
+
+    public void settLevende(){
+        levende = true;
+    }
+
+    public boolean erLevende(){
+        if (levende == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public char hentStatusTegn(){
+        if (levende == true){
+            return 'O';
+        } else {
+            return '.';
+        }
+    }
+
+    public void leggTilNabo(celle nabo) {
+        if (naboer.length < AntallNaboer){
+            naboer[AntallNaboer] = nabo;
+            AntallNaboer ++;
+        } else {
+            System.out.println("Kan ikke ha flere naboer");
+        }
+        
+    }
+
+    public void tellLevendeNaboer(){
+        antLevendeNaboer = 0;
+
+        for (int i = 0; i < AntallNaboer; i++){
+            if (naboer[i].erLevende()){
+                antLevendeNaboer ++;
+            }
+        }
+    }
+
+    public void oppdaterStatus() {
+        if (antLevendeNaboer == 3) {
+            settLevende();
+        } else {
+            settDod();
+        }
+    }
+
+
+
+    
+
+}
