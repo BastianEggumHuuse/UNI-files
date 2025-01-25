dd5bfd39b5c1828df78517e38df851f77b770ca6
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-01-25 22:30:59 +0100

    Kommentert ferdig Conway's Game of Life (Innlevering 1)
    Helt ferdig!!!

--- Bastian/SEM 2/IN1010/Innlevering_1/Celle.java
+++ Bastian/SEM 2/IN1010/Innlevering_1/Celle.java
@@ -1,10 +1,12 @@
 class Celle
 {
+    // Definerer Variabler i toppen av klassen.
     boolean Levende;
     Celle[] Naboer;
     int AntNaboer;
     int AntLevendeNaboer;
 
+    // Konstruktør for klassen Celle, som kjøres når et nytt Celle-Objekt dannes.
     public Celle()
     {
         Levende = false;
@@ -13,6 +15,7 @@ class Celle
         AntLevendeNaboer = 0;
     }
 
+    // Metoder som kontrollerer cellens "Levende"-verdi.
     public void SettDod()
     {
         Levende = false;
@@ -23,20 +26,28 @@ class Celle
         Levende = true;
     }
 
+    // Metode som brukes til å sjekke om en Celle er levende eller ikke.
     public boolean ErLevende()
     {
         return(Levende);
     }
 
+    // Metode som returnerer riktig tegn for denne cellen.
+    // Dette tegnet er '0' dersom cellen lever, og '.' dersom cellen er død.
     public char HentStatusTegn()
     {
         if (Levende)
-        {
+        {   
+            // I java brukes "" til å definere Strings, og '' til å definere Chars, altså enkeltkarakterer.
             return('0');
         }
         return('.');
     }
 
+    // Metode som legger til en nabo til denne Cellen.
+    // Disse naboene brukes til å sjekke om cellen skal leve neste generasjon
+    // Legg merke til at Cellen ikke vet noe om hvor den selv er, eller hvor naboene er.
+    // Den har bare tilgang til Naboenes Celle-objekter.
     public void LeggTilNabo(Celle NyNabo)
     {
         // Her forutsetter vi at vi ikke prøver å legge til mer enn 8 naboer 
@@ -44,12 +55,15 @@ class Celle
         AntNaboer ++;
     }
 
+    // Metode som teller antall levende naboer.
+    // Det er denne metoden vi bruker til å finne ut om Cellen skal leve. 
     public void TellLevendeNaboer()
     {
         AntLevendeNaboer = 0;
 
         for(int i = 0; i < AntNaboer; i++)
         {
+            // Her bruker vi metoden ErLevende, skrevet over, til å finne Levende-verdien til Nabocellen.
             if (Naboer[i].ErLevende())
             {
                 AntLevendeNaboer ++;
@@ -57,6 +71,7 @@ class Celle
         }
     }
 
+    // Metode som oppdaterer Cellens status, basert på spillets regler.
     public void OppdaterStatus()
     {
         if (AntLevendeNaboer == 3)
