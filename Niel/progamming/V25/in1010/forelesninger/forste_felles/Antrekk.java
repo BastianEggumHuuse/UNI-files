7c4b6c6dd5f696514c0cfdb076e265dd14a1209d
niel-carlsen <carlsenniel2005@gmail.com>
2025-02-03 11:48:15 +0100

    endrett navn bare T^T

---
+++ Niel/progamming/V25/in1010/forelesninger/forste_felles/Antrekk.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Antrekk {
+    private ArrayList<String> anledninger;
+    private ArrayList<Plagg> plagg;
+
+    public Antrekk (ArrayList<Plagg> plagg, String anledning) {
+        this.plagg = plagg;
+        anledninger = new Arraylist<>();
+        anledninger.add(anledning);
+
+        for (Plagg p : this.plagg) {
+
+        }
+
+
+    }
+}
---
+++ Niel/progamming/V25/in1010/forelesninger/forste_felles/Kategori.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList; 
+
+public class Kategori {
+    private String navn;
+    private ArrayList<Plagg> plagg;
+
+    public Kategori(String navn) {
+        this.navn = navn;
+        plagg = new ArrayList<>(); //kan skrive <String> men trenger ikke.
+    }
+
+    public void nyttPlagg(ArrayList<Plagg> farger) {
+        Plagg p = new Plagg(farger);
+        plagg.add(p);
+    }
+
+    public ArrayList<Plagg> finnPlaggMedFarge(String farge) {
+        ArrayList<Plagg> liste = new ArrayList<>();
+
+        for (Plagg p : plagg) {  //sjekke om Plagg p er i plagg array
+            if (p.harFarge(farge)) {
+                liste.add(p);
+            }
+        }
+        return liste;
+    }
+
+    public Plagg trekkeTilfeldigPlagg(String farge) {
+        ArrayList<Plagg> liste = finnPlaggMedFarge(farge);
+
+        if (liste.length != 0) {  // kan gjøre : !liste.isEmpty()
+            Random tilfeldig = new Random();
+        }
+
+    }
+
+
+
+}
