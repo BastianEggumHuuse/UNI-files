93e997c9ead7cd0948ee1b05dc6094613a59ef1d
niel-carlsen <carlsenniel2005@gmail.com>
2025-02-10 12:55:29 +0100

    felles

---
+++ Niel/progamming/V25/in1010/forelesninger/andre_felles/insekt.java
@@ -0,0 +1,10 @@
+import java.ultil.HashSet;
+
+
+public abstract class Insekt extends Dyr {
+    public final static String KLASSE = "Insecta";
+
+    public Insekt(HashSet<String> kjennetegn) {
+        super(kjennetegn);
+    }
+}
