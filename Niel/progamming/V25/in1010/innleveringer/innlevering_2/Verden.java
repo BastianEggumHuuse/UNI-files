f363fe456f9a4211924248da64a56f8a5ca5b222
niel-carlsen <carlsenniel2005@gmail.com>
2025-02-15 16:49:36 +0100

    comment

--- Niel/progamming/V25/in1010/innleveringer/innlevering_2/Verden.java
+++ Niel/progamming/V25/in1010/innleveringer/innlevering_2/Verden.java
@@ -1,49 +1,48 @@
 class Verden {
-    Rutenett rutenett;
-    int genNr;
-    
-    public Verden (int rader,int kolonner) {
-        rutenett = new Rutenett(rader,kolonner);
-        genNr = 0;
-        rutenett.fyllMedTilfeldigeCeller();
-        rutenett.kobleAlleCeller();
+
+    // lage rutenettet som representerer cellene i verden og generasjonsnummer
+    Rutenett rutenett; 
+    int genNr; 
+
+    // Konstruktør som initialiserer verden
+    public Verden(int rader, int kolonner) {
+        rutenett = new Rutenett(rader, kolonner); // Oppretter et nytt rutenett.
+        genNr = 0; // Setter generasjonsnummeret til 0.
+        rutenett.fyllMedTilfeldigeCeller(); // Fyller rutenettet med tilfeldige celler.
+        rutenett.kobleAlleCeller(); // Knytter alle celler til sine naboer.
     }
 
+    // Tegner nåværende tilstand i verden med generasjonstall og antall levende celler.
     public void tegn() {
-        System.out.println("Generasjon: "+ genNr);
-        rutenett.tegnRutenett();
-        int antallLevende = rutenett.antallLevende();
+        System.out.println("");
+        System.out.println("-------------");
+        System.out.println("Generasjon: " + genNr);
+        rutenett.tegnRutenett(); // Tegner rutenettet.
+        int antallLevende = rutenett.antallLevende(); // Teller antall levende celler.
         System.out.println(antallLevende);
-
     }
 
+    // Oppdaterer tilstanden i verden til neste generasjon.
     public void oppdatering() {
-        int antRader = rutenett.antRader;
-        int antKolonner = rutenett.antKolonner;
-        
+        int antRader = rutenett.antRader; // Henter antall rader i rutenettet.
+        int antKolonner = rutenett.antKolonner; // Henter antall kolonner i rutenettet.
+
+        // Første gjennomgang: Tell levende naboer for hver celle.
         for (int r = 0; r < antRader; r++) {
             for (int k = 0; k < antKolonner; k++) {
-
-                Celle celle = rutenett.hentCelle(r,k);
-
-                celle.tellLevendeNaboer();
-
-                
-                
+                Celle celle = rutenett.hentCelle(r, k);
+                celle.tellLevendeNaboer(); // Teller hvor mange av cellens naboer som lever.
             }
         }
 
+        // Andre gjennomgang: Oppdater statusen til hver celle basert på naboene.
         for (int r = 0; r < antRader; r++) {
             for (int k = 0; k < antKolonner; k++) {
-
-                Celle celle = rutenett.hentCelle(r,k);
-
-                celle.oppdaterStatus();
-                                
+                Celle celle = rutenett.hentCelle(r, k);
+                celle.oppdaterStatus(); // Oppdaterer cellens status i henhold til reglene.
             }
         }
 
-        genNr += 1;
-
+        genNr += 1; // Øker generasjonsnummeret med 1
     }
 }
--- Niel/progamming/V25/in1010/innleveringer/innlevering_2/celle.java
+++ Niel/progamming/V25/in1010/innleveringer/innlevering_2/celle.java
@@ -1,25 +1,28 @@
 class Celle {
 
-    boolean levende;
-    Celle[] naboer;
-    int AntallNaboer;
-    int antLevendeNaboer;
+    boolean levende; // Variabel til å lagre om cellen er levende.
+    Celle[] naboer; // Array for å lagre naboceller.
+    int AntallNaboer; // Variabel til å telle antall naboer.
+    int antLevendeNaboer; // Variabel til å telle antall levende naboer.
 
     public Celle(){
-        levende = false;
-        AntallNaboer = 0;
-        antLevendeNaboer = 0;
-        naboer = new Celle[8];
+        levende = false; // Setter cellen som død ved initiering.
+        AntallNaboer = 0; // Initierer antall naboer til 0.
+        antLevendeNaboer = 0; // Initierer antall levende naboer til 0.
+        naboer = new Celle[8]; // Skaper en array for å lagre inntil 8 naboer.
     }
 
+    // Setter statusen til cellen som død.
     public void settDod(){
         levende = false;
     }
 
+    // Setter statusen til cellen som levende.
     public void settLevende(){
         levende = true;
     }
 
+    // Returnerer true hvis cellen er levende, ellers false.
     public boolean erLevende(){
         if (levende == true){
             return true;
@@ -28,6 +31,7 @@ class Celle {
         }
     }
 
+    // Returnerer 'O' hvis cellen er levende og '.' hvis den er død.
     public char hentStatusTegn(){
         if (levende == true){
             return 'O';
@@ -36,26 +40,28 @@ class Celle {
         }
     }
 
+    // Legger til en nabocelle til naboer-arrayen hvis det er plass.
     public void leggTilNabo(Celle nabo) {
         if (naboer.length > AntallNaboer){
             naboer[AntallNaboer] = nabo;
-            AntallNaboer ++;
+            AntallNaboer++;
         } else {
             System.out.println("Kan ikke ha flere naboer");
         }
-        
     }
 
+    // Teller antall levende naboer ved å iterere gjennom naboer-arrayen.
     public void tellLevendeNaboer(){
         antLevendeNaboer = 0;
 
         for (int i = 0; i < AntallNaboer; i++){
             if (naboer[i].erLevende()){
-                antLevendeNaboer ++;
+                antLevendeNaboer++;
             }
         }
     }
 
+    // Oppdaterer statusen til cellen; blir levende hvis den har nøyaktig 3 levende naboer, ellers blir den død.
     public void oppdaterStatus() {
         if (antLevendeNaboer == 3) {
             settLevende();
@@ -63,9 +69,4 @@ class Celle {
             settDod();
         }
     }
-
-
-
-    
-
 }
