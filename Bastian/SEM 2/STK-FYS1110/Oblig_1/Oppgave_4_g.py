ee6de203b6ff9d1f6f45b2f440b593a9df4c35bf
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-02-27 23:35:47 +0100

    Gjort ferdig STK-oblig 1

--- Bastian/SEM 2/STK-FYS1110/Oblig_1/Oppgave_4_g.py
+++ Bastian/SEM 2/STK-FYS1110/Oblig_1/Oppgave_4_g.py
@@ -1,31 +1,13 @@
-from scipy.stats import binom
-import numpy as np
-import matplotlib.pyplot as plt
-# Vi har 4 ulike valg, og q antall valg vi skal gjøre:
+# Utrykket vi fant i oppgave f)
+def p(N,n,q):
+    return(1/(1+(N-n)/4**q))
 
-def n(q):
-    return 4**q
-
-# Vi skal finne sannsynligheten for akkurat en kombinasjon
-
-def P_x_s(n):
-    # n er antall kombinasjoner
-    return(1/n)
-
+# Variabler fra oppgaven
+N = 5500000
+n = 30000
 Q = [5,10,20]
-N = 30000
 
-print()
-
-#plt.title("Sannsynlighet for X ")
-
-i = 1
+print(f"\nVi har N = {N}, og n = {n}\n")
 for q in Q:
-    E = int(N*(1/4**(q)))
-    plt.subplot(3,1,i)
-    X = np.arange(E - 20,E + 20)
-    plt.bar(X,binom.pmf(X,N,1/(4**q)),width = 1,edgecolor="black",color = "lightgreen")
-    i += 1
-    #pass
-
-plt.show()
+        print(f"Naar q = {q:2}, har vi at P(X = 1|C) = {p(N,n,q):.6f}")
+print()
---
+++ Bastian/SEM 2/STK-FYS1110/Oblig_1/Oppgave_4_g_output.py
@@ -0,0 +1,9 @@
+'''
+
+Vi har N = 5500000, og n = 30000
+
+Naar q =  5, har vi at P(X = 1|C) = 0.000187
+Naar q = 10, har vi at P(X = 1|C) = 0.160860
+Naar q = 20, har vi at P(X = 1|C) = 0.999995
+
+'''
