6b2bf1b305e5bb539a02c48f180d63240db82dc4
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-03-02 21:16:42 +0100

    Oppdatert litt ting

---
+++ Bastian/SEM 2/IN1010/Innlevering_3/LenkeListe.java
@@ -0,0 +1,29 @@
+
+abstract class LenkeListe <E> implements Liste<E> 
+{
+    private class Node<T>
+    {
+        private T element;
+        private Node<T> prevElement;
+        private Node<T> nextElement;
+
+        public Node(T newElement,Node<T> pElement, Node<T> nElement)
+        {
+            element = nyttElement;
+            prevElement = pElement;
+            nextElement = nElement;
+        }
+
+        public T getElement()
+        {
+            return(element);
+        }
+    }
+
+    private Node<E> firstElement
+
+    void AddElement(E newElement,x newIndex)
+    {
+
+    }
+}
