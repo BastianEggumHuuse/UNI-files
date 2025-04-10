5e9ad653ad1f96c9d57a6c188a94e07849271ef3
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-04-10 17:39:46 +0200

    Revert "balls"
    This reverts commit b77dd434f8e1939d84cbb3dbdf76207f642e06d6.

---
+++ Niel/progamming/V25/in1010/innleveringer/innlevering_2/narkotisk.java
@@ -0,0 +1,16 @@
+public class narkotisk {
+    public final String navn;
+    public int pris;
+    public final double mengdeVirkestoff;
+    public final int styrke;
+    public final int ID;
+
+    public static void main(String navn,int pris, double mengdeVirkestoff,int styrke) {
+
+        this.navn = navn;
+        this.pris = pris;
+        this.mengdeVirkestoff = mengdeVirkestoff;
+        this.styrke = styrke;
+
+    }
+}
