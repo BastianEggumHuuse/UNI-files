5fb205c9afa2deb323df1cf3aacad8316cb55351
Bastian Eggum Huuse <Bastian.Eggum.Huuse@gmail.com>
2025-09-11 20:00:21 +0200

    Finished IN2010 Oblig 1

--- Bastian/SEM 3/IN2010/Oblig 1/TestB1.java
+++ Bastian/SEM 3/IN2010/Oblig 1/TestB1.java
@@ -8,37 +8,34 @@ class TestB1
         
         try {
 
-            String Path1 = "B1Outputs/";
-            String Path2 = "ExpectedOutputs/";
+            String Path1 = "1B_output_100000";
+            String Path2 = "ExpectedOutputs/output_100000";
 
-            for(File data : new File(Path1).listFiles())
+            // Reading
+            Scanner reader1 = new Scanner(new File(Path1));
+            Scanner reader2 = new Scanner(new File(Path2));
+
+            System.out.println("Reading file " + Path1 + "...");
+
+            // Collecting all commands;
+            int c = 0;
+            while (reader1.hasNextLine()) 
             {
-                // Reading
-                Scanner reader1 = new Scanner(data);
-                Scanner reader2 = new Scanner(new File(Path2 + data.getName()));
+                String Value1 = reader1.nextLine();
+                String Value2 = reader2.nextLine();
 
-                System.out.println("Reading file " + data.getName() + "...");
-
-                // Collecting all commands;
-                int c = 0;
-                while (reader1.hasNextLine()) 
+                if(Value1.equals(Value2) == false)
                 {
-                    String Value1 = reader1.nextLine();
-                    String Value2 = reader2.nextLine();
-
-                    if(Value1.equals(Value2) == false)
-                    {
-                        System.out.println("Something is wrong in line " + c + "!!");
-                        System.out.println(Value1 + " | " + Value2);
-                        return;
-                    }
-                    c += 1;
+                    System.out.println("Something is wrong in line " + c + "!!");
+                    System.out.println(Value1 + " | " + Value2);
+                    return;
                 }
-
-                // Finished reading
-                reader1.close();
-                reader1.close();
+                c += 1;
             }
+
+            // Finished reading
+            reader1.close();
+            reader1.close();
 
             System.out.println("\nGone through all files, no discrepancies");
         } 
